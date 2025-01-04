ed5a8bd1673e275841a2f31d5d2acce4c32fbf98
amt-ishaqueappiah <ishaque.appiah@amalitech.com>
2025-01-04 09:51:46 +0000

    raw data to write code

--- src/peak/colums/Peak_Columns.java
+++ src/peak/colums/Peak_Columns.java
@@ -1,7 +1,56 @@
 package peak.colums;
+import java.util.ArrayList;
 
 public class Peak_Columns {
+
+static int[][] matrixA ={{1,8,3,4,0},{6,7,2,7,0}};
+static ArrayList<Integer> maxValues= new ArrayList<>() ;
+static  ArrayList<Integer> minValues = new ArrayList<>();
+static int max=0;
+static  int min=Integer.MAX_VALUE;
+
+   public static void getRowMaxValues(){
+       for(int[] array:matrixA ){
+            for(int item: array){
+                if(item>max){
+                    max=item;
+                }
+                System.out.print(item +" ");
+            }
+            maxValues.add(max);
+            max=0;
+           System.out.println();
+       }
+       System.out.println(maxValues);
+    }
+
+    public static void getPeakColums(){
+       for(int val:maxValues){
+           for (int[] array:matrixA){
+               for (int newVal=0; newVal<array.length;newVal++){
+                   for(int j=0;j<maxValues.size();j++){
+                       if(val< matrixA[j][newVal] && !minValues.contains(val)){
+                           if(min>val){
+                               min=val;
+                               minValues.add(min);
+                           }
+
+                       }
+
+                   }
+
+               }
+
+           }
+       }
+        System.out.println(minValues);
+    }
+
+
     public static void main(String[] args) {
-        System.out.println("hello");
+
+            getRowMaxValues();
+            getPeakColums();
+
     }
 }
